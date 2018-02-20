52297b08825afdae86ddfbbe8bacf60b6bad9374
davide.rosselli <davide.rosselli@marconirovereto.it>
2018-02-21 00:18:15 +0100

    abbozzata funzione storie (funzionante)

--- app.js
+++ app.js
@@ -30,41 +30,40 @@ let parse_HTML = {
 }
 
 bot.onText(/\/(.+)/, function (msg, match) {
-    console.log("Received:" + msg.text + "; From usrID:" + msg.from.id + "; whose names is:" + msg.from.id + "; From usrID:" + msg.from.id + "; In chat:" + msg.chat.id);
-    let chatId = msg.chat.id;
+    let chat_id = msg.chat.id;
     let msgTxt = msg.text.toString();
     let msg_id = msg.message_id;
     let usr_id = msg.from.id;
+    console.log(`Received:${msgTxt}; From userid:${usr_id}; Firstname:${msg.from.first_name}; In chat:${msg.chat.id}`);
     //abbreviazione da usare sul sendmessage per rispondere al messaggio
     let reply = {
         reply_to_message_id: msg.message_id
     };
 
-
     let arr = match[1].split(' ');
     let command = arr[0].toString();
     let arg = arr[1]
     let opt; //opzioni comandi [-g -i]
-    console.log(match[1]);
+    console.log("command: "+command);
     switch (command) {
         case "pp":
-            sendTyping(chatId);
+            sendTyping(chat_id);
             proPic(arg);
             break;
         case "help":
-            sendTyping(chatId);
+            sendTyping(chat_id);
             help();
             break;
         case "help@Inst4bot":
-            sendTyping(chatId);
+            sendTyping(chat_id);
             help();
             break;
         case "adduser":
-            sendTyping(chatId);
+            sendTyping(chat_id);
             adduser(arg);
             break;
         case "st":
-            sendTyping(chatId);
+            sendTyping(chat_id);
             getUserStories(arg);
             break;
         case "settings":
@@ -83,14 +82,31 @@ bot.onText(/\/(.+)/, function (msg, match) {
             //prendo json dell'utente
             let jsonURL = "https://api.storiesig.com/stories/" + usr;
             let usrAcc = "https://www.instagram.com/" + usr;
-            //controllare json imageversion2 o video o entrambi o BOOOOHHHH
+            request({
+                url: jsonURL,
+                json: true
+            }, function (error, response, body) {
+
+                if (!error && response.statusCode === 200) {
+                    let media=[];
+                    body.items.forEach(element => {
+                        if(element.media_type==1){
+                            media.push(element.image_versions2.candidates[0].url)
+                        }
+                        else if(element.media_type==2){
+                            media.push(element.video_versions[0].url)
+                        }
+                    });
+                    bot.sendMessage(chat_id, media.toString(), reply);
+                }
+            })
             //mettere tutto in db everytime o non so sono stanco
             //magari fare un inline keyboard chiedendo all'utente di selezionare quale storia vuole vedere, o se farle mandare tutte
-            //fare una bella formattazione (singolo messaggio con multipli link + markdown)
         }
     }
-
-    //posso decisamente risparmiare e riutilizzare del codice fra questi due metodi
+// fare funzione request separata
+// gestire messaggio stories con markdown
+    //posso decisamente risparmiare e riutilizzare del codice fra questi due metodi, utilizzare db magari
 
     function proPic(usr) {
         if (usr != null) {
@@ -115,34 +131,34 @@ bot.onText(/\/(.+)/, function (msg, match) {
                     //console.log(finalURL);
                     //image name per i senza immagine profilo
                     if (finalURL.indexOf("11906329_960233084022564_1448528159") !== -1) {
-                        bot.sendMessage(chatId, "<a href=\"" + usrAcc + "\">" + usr + "\n" + "</a>" + "Non ha alcuna foto profilo", parse_HTML);
+                        bot.sendMessage(chat_id, "<a href=\"" + usrAcc + "\">" + usr + "\n" + "</a>" + "Non ha alcuna foto profilo", parse_HTML);
                     } else {
-                        bot.sendMessage(chatId, "<a href=\"" + finalURL.toString() + "\">Foto profilo </a>" + "di " + "<a href=\"" + usrAcc + "\">" + usr + "</a>", parse_HTML);
+                        bot.sendMessage(chat_id, "<a href=\"" + finalURL.toString() + "\">Foto profilo </a>" + "di " + "<a href=\"" + usrAcc + "\">" + usr + "</a>", parse_HTML);
 
                     }
 
                 }
                 if (error || response.statusCode != 200) {
-                    bot.sendMessage(chatId, "Username inesistente", reply);
+                    bot.sendMessage(chat_id, "Username inesistente", reply);
                 }
             })
         } else {
-            bot.sendMessage(chatId, "Devi passarmi uno username", reply);
+            bot.sendMessage(chat_id, "Devi passarmi uno username", reply);
         }
     }
 
     function settings(){
         if(usr_id!=privSettings.masterUser){
-            bot.sendMessage(chatId, "Cazzo troppo corto", reply);
+            bot.sendMessage(chat_id, "Cazzo troppo corto", reply);
         }
         else{
-            bot.sendMessage(chatId, "Hey boss", reply);
+            bot.sendMessage(chat_id, "Hey boss", reply);
             //fare cose
         }
     }
 
     function help() {
-        bot.sendMessage(chatId, "Lista comandi:\n/pp username - ritorna foto profilo\n/st username - ritorna stories utente", reply);
+        bot.sendMessage(chat_id, "Lista comandi:\n/pp username - ritorna foto profilo\n/st username - ritorna stories utente", reply);
     }
 })
 
